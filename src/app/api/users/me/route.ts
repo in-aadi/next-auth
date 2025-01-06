e55070cbd8bf5b6c721924e2ca50eae8bcd11d41
Aditya Sharma <3008.aditya.sharma@gmail.com>
2025-01-06 20:16:49 +0530

    added user profile route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,18 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+	const userId = await getDataFromToken(request);
+	const user = await User.findOne({ _id: userId }).select("-password");
+	if (!user) {
+		return NextResponse.json({ error: "User not found" }, { status: 404 });
+	}
+	return NextResponse.json({
+		message: "User found",
+		data: user,
+	});
+}
