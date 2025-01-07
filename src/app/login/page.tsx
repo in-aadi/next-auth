99e4e530d642b1247e828852ef7508a953c0a1ba
Aditya Sharma <3008.aditya.sharma@gmail.com>
2025-01-07 19:07:44 +0530

    added login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,72 @@
-function page() {
-  return (
-    <div>page</div>
-  )
-}
+"use client";
 
-export default page
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function LoginPage() {
+	const router = useRouter();
+	const [user, setUser] = useState({
+		email: "",
+		password: "",
+	});
+	const [buttonDisabled, setButtonDisabled] = useState(true);
+	const [loading, setLoading] = useState(false);
+
+	const onLogin = async () => {
+		try {
+			setLoading(true);
+			const response = await axios.post("/api/users/login", user);
+			console.log("Login success", response);
+			router.push("/profile");
+		} catch (error: any) {
+			console.log(error.message);
+			toast.error(error.message);
+		}
+	};
+
+	useEffect(() => {
+		if (
+			user.email.length > 0 &&
+			user.password.length > 0
+		) {
+			setButtonDisabled(false);
+		} else {
+			setButtonDisabled(true);
+		}
+	}, [user]);
+
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+			<h1>{loading ? "Processing" : "Login"}</h1>
+			<hr />
+			<label htmlFor="email">email</label>
+			<input
+				className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+				id="email"
+				type="text"
+				value={user.email}
+				onChange={(e) => setUser({ ...user, email: e.target.value })}
+				placeholder="email"
+			/>
+			<label htmlFor="password">password</label>
+			<input
+				className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+				id="password"
+				type="password"
+				value={user.password}
+				onChange={(e) => setUser({ ...user, password: e.target.value })}
+				placeholder="password"
+			/>
+			<button
+				onClick={onLogin}
+				className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+			>
+				{buttonDisabled ? "Please fill the form" : "Signup"}
+			</button>
+			<Link href="/Signup">Visit signup page</Link>
+		</div>
+	);
+}
