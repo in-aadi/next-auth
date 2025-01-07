df0e29eae25bf0654b08df534b207fc17e022d16
Aditya Sharma <3008.aditya.sharma@gmail.com>
2025-01-07 21:09:31 +0530

    add middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,35 +1,30 @@
-'use client'
-
+"use client";
 import axios from "axios";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
 	const router = useRouter();
-	const [data, setData] = useState("");
-
-	const getUserData = async () => {
-		try {
-			const res = await axios.post("/api/users/me");
-			console.log(res.data.data._id);
-			setData(res.data.data._id);
-		} catch (error: any) {
-			console.log(error.message);
-			toast.error(error.message);
-		}
-	};
-
+	const [data, setData] = useState("nothing");
 	const logout = async () => {
 		try {
 			await axios.get("/api/users/logout");
-			toast.success("logout success");
+			toast.success("Logout successful");
+			router.push("/login");
 		} catch (error: any) {
 			console.log(error.message);
 			toast.error(error.message);
 		}
 	};
+
+	const getUserDetails = async () => {
+		const res = await axios.get("/api/users/me");
+		console.log(res.data);
+		setData(res.data.data._id);
+	};
+
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen py-2">
 			<h1>Profile</h1>
@@ -51,10 +46,10 @@ export default function ProfilePage() {
 			</button>
 
 			<button
-				onClick={getUserData}
+				onClick={getUserDetails}
 				className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 			>
-				Get User Details
+				GetUser Details
 			</button>
 		</div>
 	);
